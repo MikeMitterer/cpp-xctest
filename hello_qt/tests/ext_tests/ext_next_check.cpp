cc58d2e85f4dd8e37036425ea64efce059339dfb
Mike Mitterer <office@mikemitterer.at>
2016-10-18 15:47:07 +0200

    Google-Test hinzugefügt

---
+++ hello_qt/tests/ext_tests/ext_next_check.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Mike Mitterer on 02.11.15.
+//
+
+#include "gtest/gtest.h"
+
+TEST(ext_next_check, test_eq) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(ext_next_check, test_neq) {
+    EXPECT_NE(1, 0);
+}
+
