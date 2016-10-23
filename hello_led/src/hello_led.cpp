fdf093d0f5fb969198202c0ba0ae8ee7ad213b1c
Mike Mitterer <office@mikemitterer.at>
2016-10-23 19:57:11 +0200

    HelloLed funkt am Raspi

---
+++ hello_led/src/hello_led.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "GPIO.h"
+
+using namespace mm;
+
+int main() {
+    gpio::Board board;
+
+    if (!board.init()) {
+        printf("GPIO initialization failed!\n");
+    }
+    printf("RASPI is up and running\n");
+
+    // Pin 18 / GPIO 24 muss exportiert sein (gpio export 24 in)
+    gpio::Pin pinButton(24);
+
+    // Pin 13 / GPIO 27 muss exportiert sein (gpio export 27 out)
+    // Lambda-Funktion wird im Destruktor aufgerufen!
+    gpio::Pin pinLed(27, [](gpio::Pin &pin) {
+        pin.write(gpio::Pin::OutputState::OFF);
+        printf("Turned off LED with Lambda-Function!\n");
+    });
+
+    pinLed.write(gpio::Pin::OutputState::ON);
+    mm::gpio::delay(100);
+    mm::gpio::delay(100);
+    pinLed.write(gpio::Pin::OutputState::OFF);
+
+    std::cout << "Hello, LED - Blink, Blink!" << std::endl;
+    return 0;
+}
---
+++ hello_lib/README.txt
@@ -0,0 +1,2 @@
+SSHFS Verbindung aufbauen
+    sshfs pi@192.168.0.90:/home/pi/bin `pwd`
