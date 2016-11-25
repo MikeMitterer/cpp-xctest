1dac74935e8185a9d6f8851dfee8d5ed46f20dd4
Mike Mitterer <office@mikemitterer.at>
2016-11-25 15:31:16 +0100

    Logger und ArgParser funktionieren

--- hello_world/src/hello_world.cpp
+++ hello_world/src/hello_world.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+#include "spdlog/spdlog.h"
 
-int main() {
-    std::cout << "Hello, World!!" << std::endl;
+#include <iostream>
+#include <args.hxx>
+
+namespace spd = spdlog;
+int main(int argc,const char *argv[]) {
+    auto logger = spd::stdout_color_mt("console");
+
+    args::ArgumentParser parser("This is a test program.", "This goes after the options.");
+    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+
+    args::ValueFlag<int> integer(parser, "integer", "The integer flag", {'i'});
+    args::ValueFlagList<char> characters(parser, "characters", "The character flag", {'c'});
+    args::Positional<std::string> foo(parser, "foo", "The foo position");
+    args::PositionalList<double> numbers(parser, "numbers", "The numbers position list");
+
+    logger->warn("Hallo ein Test");
+
+    try {
+        parser.ParseCLI(argc, argv);
+        if(argc == 1) {
+            throw args::Help("");
+        }
+    }
+    catch (args::Help e) {
+        std::cout << parser;
+        return 0;
+    }
+    catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+    catch (args::ValidationError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+    if (integer) { std::cout << "i: " << args::get(integer) << std::endl; }
+    if (characters) { for (const auto ch: args::get(characters)) { std::cout << "c: " << ch << std::endl; }}
+    if (foo) { std::cout << "f: " << args::get(foo) << std::endl; }
+    if (numbers) { for (const auto nm: args::get(numbers)) { std::cout << "n: " << nm << std::endl; }}
+
     return 0;
 }
